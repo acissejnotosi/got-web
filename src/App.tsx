faeb96a8c1e7abcc4f7fe07f49966ac59bb7e212
Jessica Isoton <jessicasampaio195@gmail.com>
2021-07-25 15:49:51 +0200

    Added stark sigil, logo and map

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,23 @@
 import React from "react";
 import './scss/app.scss';
-/* import gotMap from './resources/got-map.jpg';
-import gotLogo from './resources/got-logo.png'; */
-import starkHouseSigil from './resources/stark-house-sigil.png'
+import gotMap from './resources/got-map.jpg';
+import gotLogo from './resources/got-logo.png';
+import houseStarkSigil from './resources/stark-house-sigil.png'
 
 function App() {
   return (
-    <div className="map-container" id="map-container">
-      {/*   <img id="got-map" src={gotMap} alt="map" />
-      <img id="got-logo" src={gotLogo} alt="logo" />
- */}
-      <a href="/." target="_blank">
-        <div className="img-box">
-          <img id="stark-house-sigil" src={starkHouseSigil} alt="stark-house-sigil" />
+    <div className="map-container">
+      <img id="got-map" src={gotMap} alt="map" />
+      <div className="map-body">
+        <img id="got-logo" src={gotLogo} alt="logo" />
+        <div id="house-stark-sigil-container" className="img-container" >
+          <a href="/." target="_blank">
+            <div className="img-shadow">
+              <img src={houseStarkSigil} alt="house-stark-sigil" />
+            </div>
+          </a>
         </div>
-      </a>
-
+      </div>
     </div>
   );
 }
